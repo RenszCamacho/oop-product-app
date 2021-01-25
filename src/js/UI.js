f3cf7469f06dadc5e0297b932c7815adfb1df0db
Renszo Camacho <varsovia84@gmail.com>
2021-01-25 13:44:45 +0100

    Fixing a bug on the UI.js. change className to class.

--- src/js/UI.js
+++ src/js/UI.js
@@ -3,8 +3,8 @@ export class UI {
     const productList = document.getElementById("product-list");
     const element = document.createElement("div");
     element.innerHTML = `
-    <div className="card text-center mb-4">
-      <div className="card-body">
+    <div class="card text-center mb-4">
+      <div class="card-body">
         <strong>Product Name:</strong>${product.name}
         <strong>Product Price:</strong>${product.price}
         <strong>Product Year:</strong>${product.year}
